1ef5320ac2a08e0a60261e58ccca8e73350576f1
Alik Khilazhev <alikhil@mail.ru>
2018-07-04 11:09:57 +0300

    Add transforms package

---
+++ internal/pkg/transformations/transforms.go
@@ -0,0 +1,38 @@
+package transformations
+
+import (
+	"gopkg.in/h2non/bimg.v1"
+)
+
+// Fit - the image is resized so that it takes up as much space as possible
+// within a bounding box defined by the given width and height parameters.
+// The original aspect ratio is retained and all of the original image is visible.
+func Fit(buffer []byte, side, quality int) ([]byte, error) {
+	var img = bimg.NewImage(buffer)
+
+	if img.Type() != "jpg" {
+		jpg, err := img.Convert(bimg.JPEG)
+		if err != nil {
+			return nil, err
+		}
+		img = bimg.NewImage(jpg)
+	}
+
+	var sz, err = img.Size()
+	if err != nil {
+		return nil, err
+	}
+	if sz.Height > sz.Width {
+		return img.Process(bimg.Options{
+			Height:        side,
+			Quality:       quality,
+			StripMetadata: true,
+		})
+	} else {
+		return img.Process(bimg.Options{
+			Width:         side,
+			Quality:       quality,
+			StripMetadata: true,
+		})
+	}
+}
