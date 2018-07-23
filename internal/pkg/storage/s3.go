e7cee0d911f6e00c761c3268b8d0744bdb7dff65
Alik Khilazhev <alikhil@mail.ru>
2018-07-23 18:55:48 +0300

    S3 add folder deletion

--- internal/pkg/storage/s3.go
+++ internal/pkg/storage/s3.go
@@ -1,9 +1,12 @@
 package storage
 
 import (
+	"bytes"
 	"context"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"io"
 	"os"
@@ -12,13 +15,11 @@ import (
 // UploadFile - uploads the file with objectKey key
 func UploadFile(file io.Reader, objectKey string) (string, error) {
 
-	sess := session.Must(session.NewSession(&aws.Config{
-		Endpoint: aws.String(os.Getenv("S3_ENDPOINT")),
-	}))
+	sess := getSession()
 
 	manager := s3manager.NewUploader(sess)
 	out, err := manager.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(os.Getenv("S3_BUCKET")),
+		Bucket: getBucket(),
 		Body:   file,
 		Key:    aws.String(objectKey),
 		ACL:    aws.String("public-read"),
@@ -29,16 +30,14 @@ func UploadFile(file io.Reader, objectKey string) (string, error) {
 	return out.Location, err
 }
 
-// UploadFile - uploads the file with objectKey key
+// UploadFileWithContext - uploads the file with objectKey key with context
 func UploadFileWithContext(ctx context.Context, file io.Reader, objectKey string) (string, error) {
 
-	sess := session.Must(session.NewSession(&aws.Config{
-		Endpoint: aws.String(os.Getenv("S3_ENDPOINT")),
-	}))
+	sess := getSession()
 
 	manager := s3manager.NewUploader(sess)
 	out, err := manager.UploadWithContext(ctx, &s3manager.UploadInput{
-		Bucket: aws.String(os.Getenv("S3_BUCKET")),
+		Bucket: getBucket(),
 		Body:   file,
 		Key:    aws.String(objectKey),
 		ACL:    aws.String("public-read"),
@@ -47,4 +46,53 @@ func UploadFileWithContext(ctx context.Context, file io.Reader, objectKey string
 	})
 
 	return out.Location, err
+}
+
+func getBucket() *string {
+	return aws.String(os.Getenv("S3_BUCKET"))
+}
+
+func getSession() *session.Session {
+	return session.Must(session.NewSession(&aws.Config{
+		Endpoint: aws.String(os.Getenv("S3_ENDPOINT")),
+		LogLevel: aws.LogLevel(aws.LogDebugWithHTTPBody),
+	}))
+}
+
+// DeleteFolder - Deletes all files with given prefix
+func DeleteFolder(prefix string) error {
+	sess := getSession()
+
+	svc := s3.New(sess)
+
+	svc.Handlers.Build.PushBack(func(r *request.Request) {
+
+		if r.Operation.Name == "DeleteObjects" {
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(r.Body)
+			updated := bytes.Replace(buf.Bytes(), []byte(` xmlns="http://s3.amazonaws.com/doc/2006-03-01/"`), []byte(""), -1)
+			r.SetReaderBody(bytes.NewReader(updated))
+		}
+	})
+
+	objects, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: getBucket(),
+		Prefix: aws.String(prefix),
+	})
+
+	if err != nil {
+		return err
+	}
+	obIdentifiers := make([]*s3.ObjectIdentifier, len(objects.Contents))
+	for i, obj := range objects.Contents {
+		obIdentifiers[i] = &s3.ObjectIdentifier{Key: obj.Key}
+	}
+
+	_, err = svc.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: getBucket(),
+		Delete: &s3.Delete{
+			Objects: obIdentifiers,
+		},
+	})
+	return err
 }
--- internal/pkg/storage/s3_test.go
+++ internal/pkg/storage/s3_test.go
@@ -1,7 +1,10 @@
 package storage
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
@@ -23,4 +26,46 @@ func TestUploadFile(t *testing.T) {
 	}
 
 	// TODO: add cleanup
+}
+
+func TestDeleteFolder(t *testing.T) {
+	assert := assert.New(t)
+
+	file1 := "test-dir/pict1.jpg"
+	file2 := "test-dir/pict2.jpg"
+
+	godotenv.Load("../../../.env")
+
+	filename := "../../../test/data/picture.jpg"
+
+	// upload file1
+	f, ferr := os.Open(filename)
+	assert.NoError(ferr)
+
+	_, err := UploadFile(f, file1)
+	assert.NoError(err)
+
+	_, err = UploadFile(f, file2)
+	assert.NoError(err)
+
+	err = DeleteFolder("test-dir")
+
+	assert.NoError(err)
+
+	ensureFilesDeleted(t, "test-dir")
+
+}
+
+func ensureFilesDeleted(t *testing.T, prefix string) {
+	sess := getSession()
+
+	svc := s3.New(sess)
+	objects, err := svc.ListObjects(&s3.ListObjectsInput{
+		Bucket: getBucket(),
+		Prefix: aws.String(prefix),
+	})
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, 0, len(objects.Contents))
 }
