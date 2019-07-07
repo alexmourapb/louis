5bb9735324eee0db3fff9868bf19a11fe4539146
Alik Khilazhev <alikhil@mail.ru>
2019-07-07 22:43:45 +0300

    Refactor and add auth middleware

--- internal/app/louis/middlewares.go
+++ internal/app/louis/middlewares.go
@@ -9,6 +9,7 @@ import (
 	"time"
 )
 
+// NewThrottler - contsructor for Throttler
 func NewThrottler(cfg *utils.Config) *Throttler {
 	return &Throttler{
 		semaphore: semaphore.NewWeighted(cfg.ThrottlerQueueLength),
@@ -16,27 +17,29 @@ func NewThrottler(cfg *utils.Config) *Throttler {
 	}
 }
 
+// Throttler - simple middleware to throttle traffic
 type Throttler struct {
 	semaphore *semaphore.Weighted
 	timeout   time.Duration
 }
 
 // Lock - tries to acquire right to handle request
-func (t *Throttler) Lock() bool {
+func (t *Throttler) lock() bool {
 	ctx, cancel := context.WithTimeout(context.Background(), t.timeout)
 	defer cancel()
 	var err = t.semaphore.Acquire(ctx, 1)
 	return err == nil
 }
 
-func (t *Throttler) Unlock() {
+func (t *Throttler) unlock() {
 	t.semaphore.Release(1)
 }
 
+// Throttle - locks until request can be handled or timeout
 func (t *Throttler) Throttle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if t.Lock() {
-			defer t.Unlock()
+		if t.lock() {
+			defer t.unlock()
 			next.ServeHTTP(w, r)
 		} else {
 			err := respondWithJSON(w, "too many requests", nil, 503)
@@ -46,4 +49,16 @@ func (t *Throttler) Throttle(next http.Handler) http.Handler {
 			}
 		}
 	})
+}
+
+// Authorize - creates authorization middleware by given key
+func Authorize(key string) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var header = r.Header.Get("Authorization")
+			if header != key {
+				respondWithJSON(w, "account not found", nil, http.StatusUnauthorized)
+			}
+		})
+	}
 }
